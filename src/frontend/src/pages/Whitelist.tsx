65e3a1fa450be76ed35543d1f47585aec7ec1f60
Flaxelaxen <mrkaland95@gmail.com>
2024-11-23 06:55:25 +0100

    Whitelist page mostly complete.

--- src/frontend/src/pages/Whitelist.tsx
+++ src/frontend/src/pages/Whitelist.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useQuery } from '@tanstack/react-query';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {IPrivilegedRole} from "../../../../dist/backend/schema";
 
 export type WhitelistResponseData = {
@@ -14,42 +14,15 @@ export type WhitelistResponseData = {
     }[],
 }
 
-
-async function fetchUsersWhitelists(): Promise<WhitelistResponseData> {
-    // const reqEndpoint = "http://localhost:5000/api/profile/user"
-    const res = await axios({
-        method: "GET",
-        url: "http://localhost:5000/api/profile/user",
-        withCredentials: true
-    })
-
-    if(res.status != 200) {
-        throw new Error("Unable to fetch whitelist data.")
-    }
-
-    return res.data
+type WhitelistFormProps = {
+    whitelist: WhitelistRow[]
+    whitelistSlots: number,
+    onSubmit: (data: WhitelistRow[]) => void
 }
 
-
-function WhiteListForms(data: WhitelistResponseData) {
-    const testData = [
-        'test1',
-        'test2',
-        'test3',
-        'test4',
-    ]
-
-    // console.alert("")
-
-
-    for (const d of testData) {
-        console.log(d)
-    }
-
-    // console.log(data)
-    return <div>
-        <p>{data.whitelistSlots}</p>
-    </div>
+type WhitelistRow = {
+    steamID: string
+    name?: string
 }
 
 
@@ -69,7 +42,7 @@ function Whitelist() {
 
     return (
     <>
-        <div className={"whitelist-body"}>
+        <div className={"whitelist-container"}>
             <h1>
                 Whitelist
             </h1>
@@ -77,15 +50,120 @@ function Whitelist() {
                 On this page you can add other people's steamIDs to whitelist.
             </h3>
             <p>You currently have {data.whitelistSlots} whitelist slots available<br/>
+            </p>
+            <p style={{paddingTop: '5px', paddingBottom: '5px'}}>
             Which are active the follow days: {data.whitelistActiveDays}
             </p>
-            {/*<p>{data}</p>*/}
+            <br/>
+            <div className={"whitelist-forms"}>
+                <WhiteListForms whitelist={data.whitelistedSteam64IDs} whitelistSlots={data.whitelistSlots} onSubmit={onFormSubmit}></WhiteListForms>
+            </div>
         </div>
-        <div className={"whitelist-forms"}>
-            <WhiteListForms {...data}></WhiteListForms>
-        </div>
+
     </>
     )
 }
+
+
+function WhiteListForms({whitelist, whitelistSlots, onSubmit}: WhitelistFormProps) {
+    const [whitelistRows, setWhitelistRows] = useState<WhitelistRow[]>([])
+
+    useEffect(() => {
+        let slots: WhitelistRow[] = []
+        for (let i = 0; i < whitelist.length || i < whitelistSlots; i++) {
+            let row: WhitelistRow = { steamID: '', name: '' }
+            const steamID = whitelist[i]?.steamID
+            const name = whitelist[i]?.name
+            // We only want to store the name if there is a steamID
+            if (steamID) {
+                row.steamID = steamID
+                if (name) {
+                    row.name = name
+                }
+            }
+            slots.push(row)
+            setWhitelistRows([...slots])
+        }
+    }, [whitelist, whitelistSlots])
+
+
+
+
+    function handleInputChange(index: number, field: 'steamID' | 'name', value: string) {
+        const updatedRows = [...whitelistRows]
+        updatedRows[index][field] = value;
+        setWhitelistRows(updatedRows)
+    }
+
+
+    // TODO this wills end a post request to the server with the steamID, which will then send an API request to steam to see if the steamID is valid.
+    function validateSteamID(steamID: string) {
+
+    }
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        onSubmit(whitelistRows);
+    }
+
+    return(
+    <form onSubmit={handleSubmit}>
+        <label>Whitelist Slots</label>
+        {whitelistRows.map((row, index) => (
+            <div key={index} className={"whitelist-row"} style={{display: 'flex',justifyContent: 'center', alignItems: 'center', marginBottom: '10px'}}>
+                <input
+                    type={"text"}
+                    value={row.steamID}
+                    onChange={(e) => handleInputChange(index, 'steamID', e.target.value)}
+                    placeholder={"Enter SteamID"}
+                    style={{marginRight: '10px'}}
+                    // required={true}
+                />
+                <input
+                    type={"text"}
+                    value={row.name}
+                    onChange={(e) => handleInputChange(index, 'name', e.target.value)}
+                    placeholder={"Enter Optional Name"}
+                    style={{marginRight: '10px'}}
+                />
+                <button
+                    type={"button"}
+                    onClick={() => validateSteamID(row.steamID)}
+                    style={{marginRight: '10px'}}
+                >
+                    Check SteamID
+                </button>
+            </div>
+        ))}
+        <div style={{display: 'flex', justifyContent: "center"}}>
+            <button type={"submit"} style={{marginTop: '20px'}} className={"default-button"}>Submit</button>
+        </div>
+    </form>
+    )
+}
+
+async function fetchUsersWhitelists(): Promise<WhitelistResponseData> {
+    const res = await axios({
+        method: "GET",
+        url: "http://localhost:5000/api/profile/user",
+        withCredentials: true
+    })
+
+    if(res.status != 200) {
+        throw new Error("Unable to fetch whitelist data.")
+    }
+
+    return res.data
+}
+
+async function onFormSubmit(data: WhitelistRow[]) {
+
+
+
+
+    console.log("Sumbit data", data)
+
+}
+
 
 export default Whitelist
