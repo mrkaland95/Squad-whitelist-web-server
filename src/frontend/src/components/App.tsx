c5a72172ac8ec4aa8b0b9ddbd5a553fbbb569f09
Flaxelaxen <mrkaland95@gmail.com>
2024-11-23 06:54:35 +0100

    Refactor, added useQuery wrapper

--- src/frontend/src/components/App.tsx
+++ src/frontend/src/components/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import logo from '../logo.svg';
-import '../css/App.css';
+import '../css/styles.css';
 import DiscordAuthentication from "./Login";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import Home from "./Home";
+import Home from "../pages/Home";
 import {Route, Routes} from "react-router-dom";
-import Profile from "./not-implemented/Profile";
-import Admin from "./not-implemented/Admin";
-import Whitelist from "./not-implemented/Whitelist";
+import Profile from "../pages/Profile";
+import User from "../pages/User";
+import Whitelist from "../pages/Whitelist";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+  <QueryClientProvider client={queryClient}>
     <div className="app-container">
         <Navbar></Navbar>
         <div className="body">
@@ -20,12 +25,13 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/profile" element={<Profile />} />
-                    <Route path="/user" element={<Admin />} />
+                    <Route path="/user" element={<User />} />
                     <Route path="/whitelist" element={<Whitelist />} />
                 </Routes>
             </div>
         </div>
     </div>
+  </QueryClientProvider>
   );
 }
 
