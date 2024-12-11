75ab1f29a8770445ff7469d574e48fd3d3d11c95
Flaxelaxen <mrkaland95@gmail.com>
2024-12-11 18:22:40 +0100

    Add functionality for fetching groups, discord roles

--- src/frontend/src/utils/fetch.ts
+++ src/frontend/src/utils/fetch.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import {AdminGroupRow} from "../pages/AdminGroups";
 import {WhitelistResponseData, WhitelistRow} from "../pages/Whitelist";
 import {IDiscordUser} from "../pages/User";
+import {responseData} from "../pages/Profile";
+import {IPrivilegedRole} from "../pages/RoleEdit";
 
 
 const baseURL = "http://localhost:5000";
@@ -9,7 +11,7 @@ const baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 
 export async function getAdminGroups() {
-    const url = `${baseURL}/api/v1/admingroups/groups`
+    const url = `${baseURL}/api/v1/admingroups`
     const res = await axios.get(url)
 
     if (res.status != 200) {
@@ -19,12 +21,23 @@ export async function getAdminGroups() {
     return res.data
 }
 
+export async function getAllDiscordRoles() {
+    const url = `${baseURL}/api/v1/allroles`
+    const res = await axios.get(url)
+
+    if (res.status != 200) {
+        throw new Error(`Unable to fetch discord roles`)
+    }
+
+    return res.data
+}
 
 export async function postAdminGroups(adminGroups: AdminGroupRow[]) {
+
     return await axios.post(
-        `${baseURL}/api/v1/groups`,
+        `${baseURL}/api/v1/admingroups`,
         {
-            adminGroups
+            adminGroupRows: adminGroups
         }
     )
 }
@@ -59,4 +72,48 @@ export async function postUserSteamID(steamID: string) {
 
 export async function getUserSteamID() {
     return axios.get(`${baseURL}/api/v1/user/userid`)
+}
+
+export async function getPrivilegedDiscordRoles() {
+    const res = await axios.get(`${baseURL}/api/v1/roles`)
+
+    if(res.status != 200) {
+        throw new Error("Unable to fetch privileged discord roles")
+    }
+
+    return res.data
+}
+
+export async function postPrivilegedDiscordRoles(roles: IPrivilegedRole[]) {
+    const result = await axios.post(`${baseURL}/api/v1/roles`, roles)
+
+    if (result.status != 200) {
+        throw new Error("Unable to fetch privileged discord roles")
+    }
+
+    return result
+}
+
+export async function getListEndpoints() {
+    const url = `${baseURL}/api/v1/listedit`
+    const res = await axios.get(url)
+
+    if (res.status != 200) {
+        throw new Error(`Unable to fetch admin group data`)
+    }
+
+    return res.data
+}
+
+
+export async function fetchUserData(): Promise<responseData> {
+    const response = await fetch(`${baseURL}/v1/user/userinfo`, {
+        credentials: "include"
+    })
+
+    if (!response.ok) {
+        throw new Error('Unable to fetch profile data.')
+    }
+
+    return response.json()
 }
