65a55e70bf5f7779e448ebaf9428b7404c7721a2
Flaxelaxen <mrkaland95@gmail.com>
2024-12-11 18:12:20 +0100

    Add shared type file.

--- src/shared-types/types.d.ts
+++ src/shared-types/types.d.ts
@@ -1,7 +1,15 @@
+
+/**
+ * This file defines types that are shared between the frontend and backend.
+ */
+
+
 /*
 Pulled and defined from:
 https://squad.fandom.com/wiki/Server_Administration
  */
+
+
 export enum InGameAdminPermissions {
     CHANGE_MAP = "changemap",
     CAN_SEE_ADMIN_CHAT = "canseeadminchat",
@@ -22,3 +30,71 @@ export enum InGameAdminPermissions {
     DEBUG = "debug",
     TEAM_CHANGE = "teamchange"
 }
+
+/*
+A week day corresponding to a number, particularly to inbuilt Javascript
+Date.getDay() method.
+ */
+export enum WeekDays {
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6
+}
+
+export interface DiscordUser {
+    DiscordID: string;
+    DiscordName: string;
+    Roles: string[];
+    Whitelist64IDs: { steamID: string; name?: string }[];
+    UserID64?: { steamID: string; isLinkedToSteam: boolean};
+    Enabled: boolean;
+}
+
+export interface AdminGroup {
+    GroupID: string;
+    GroupName: string,
+    Permissions: [InGameAdminPermissions],
+    Enabled: boolean,
+    IsWhitelistGroup: boolean
+}
+
+
+export interface Lists {
+    ListName: string,
+    AdminGroups: [AdminGroup],
+    AllRolesEnabled: boolean,
+    UseWhitelistGroup: boolean,
+    Enabled: boolean
+}
+
+
+export interface PrivilegedRole {
+    RoleID: string,
+    RoleName: string,
+    AdminGroup?: AdminGroup,
+    ActiveDays: [WeekDays],
+    WhitelistSlots: number
+    Enabled: boolean
+}
+
+
+export interface Log {
+    LogMessage: string,
+    MessageType?: string,
+}
+
+
+export interface DiscordRole {
+    RoleID: string,
+    RoleName: string,
+    GuildID: string
+}
+
+export interface APIKey {
+    APIKey: string
+}
+
